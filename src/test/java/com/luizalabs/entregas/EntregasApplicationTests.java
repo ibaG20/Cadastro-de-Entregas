1353f29fba001b02f6f18552f8fe8e8d25b4a639
ibaG20 <marretogabriella@gmail.com>
2024-11-22 19:09:10 -0300

    estrutura inicial do projeto

---
+++ src/test/java/com/luizalabs/entregas/EntregasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luizalabs.entregas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntregasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

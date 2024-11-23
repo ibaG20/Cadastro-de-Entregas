9855d732f13dd2bb962f9b0df120b5358585a90c
ibaG20 <marretogabriella@gmail.com>
2024-11-22 23:04:58 -0300

    Criação da estrutura das classes de controller e service (também para testes)

---
+++ src/main/java/com/luizalabs/entregas/controller/EntregaController.java
@@ -0,0 +1,5 @@
+package com.luizalabs.entregas.controller;
+
+public class EntregaController {
+
+}
---
+++ src/test/java/com/luizalabs/entregas/service/EntregaServiceTest.java
@@ -0,0 +1,5 @@
+package com.luizalabs.entregas.service;
+
+public class EntregaServiceTest {
+
+}
